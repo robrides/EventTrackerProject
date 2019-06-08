dbd0ac9fd0e07e5cbc6fd9c369ca2cb5ad01e5a3
robrides <techjunkey@gmail.com>
2019-06-08 11:31:39 -0600

    App is fully functional. Deployed to AWS and tested

--- RideTrackerREST/src/main/resources/static/js/script.js
+++ RideTrackerREST/src/main/resources/static/js/script.js
@@ -4,7 +4,6 @@ window.addEventListener('load', function(e) {
 	});
 
 	function init() {
-		console.log('get all rides in init');
 	  getAllRides();
 		
 	  document.rideForm.lookup.addEventListener('click', function(event) {
@@ -73,14 +72,15 @@ window.addEventListener('load', function(e) {
 	        }
 		   };
 		    updateRide(rideObj);
-		    rideEdit.reset();
+		    document.getElementById('rideEdit').reset();
+		    document.getElementById('editRideDiv').innerHTML = "";
 		  })
 		  
 		  document.rideEdit.rideDelete.addEventListener('click', function(e){
 			  if(confirm("Confirm Delete")){	  
 			  deleteRide();
-			  let rideEditForm = document.getElementByTagName('rideEdit');
-			  rideEditForm.reset();
+			  document.getElementById('rideEdit').reset();
+			  document.getElementById('editRideDiv').innerHTML = "";
 			  } else {
 				  alert("Not Deleted");
 			  }
@@ -90,7 +90,6 @@ window.addEventListener('load', function(e) {
 
 	function updateRide(rideObj) {
 		var xhr = new XMLHttpRequest();
-		console.log(rideObj);
 		xhr.open('PUT', 'api/rides/' + rideObj.id, true);
 		xhr.setRequestHeader("Content-type", "application/json");
 
@@ -112,7 +111,6 @@ window.addEventListener('load', function(e) {
 	
 	function addRide(rideObj) {
 		var xhr = new XMLHttpRequest();
-		console.log(rideObj);
 		xhr.open('POST', 'api/rides/', true);
 		xhr.setRequestHeader("Content-type", "application/json");
 
@@ -120,6 +118,7 @@ window.addEventListener('load', function(e) {
 			if (xhr.readyState === 4 && xhr.status < 400) {
 				var rideAdded = JSON.parse(xhr.responseText);
 				getAllRides();
+			    document.getElementById('rideAdd').reset();
 			};
 
 			if (xhr.readyState === 4 && xhr.status >= 400) {
@@ -157,7 +156,6 @@ window.addEventListener('load', function(e) {
 
 	}
 	function deleteRide() {
-		console.log('in deleteRide');
 		let rideEditForm = document.getElementById('rideEdit');
 		let rideId = rideEditForm.id.value;
 		
@@ -168,10 +166,12 @@ window.addEventListener('load', function(e) {
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState === 4 && xhr.status < 400) {
 				var ride = JSON.parse(xhr.responseText);
-				console.log(ride);
 				let dataDiv = document.getElementById('rideData');
 				dataDiv.textContent = 'Ride ' + rideId + ' deleted';
 				getAllRides();
+				document.getElementByTagName('rideEdit').reset();
+				document.getElementById('editRideDiv').textContent = '';
+				
 			};
 			
 			if (xhr.readyState === 4 && xhr.status >= 400) {
@@ -190,14 +190,11 @@ window.addEventListener('load', function(e) {
 			var xhr = new XMLHttpRequest();
 
 			xhr.open('GET', 'api/rides', true);
-			console.log('get all rides func open');
 
 			xhr.onreadystatechange = function() {
-				console.log('get all rides func ready');
 				if (xhr.readyState === 4 && xhr.status < 400) {
 					var rides = JSON.parse(xhr.responseText);
 					displayAllRides(rides);
-					console.log('display all call at xhr');
 					
 				};
 
@@ -217,84 +214,57 @@ window.addEventListener('load', function(e) {
 		ridesList.textContent = '';
 		
 		  var tbl = document.createElement('table');
+		  let hrTotal = 0;
+		  let count = 0;
+		  
+		  let fRow = document.createElement('tr')
+		  let fData1 = document.createElement('td');
+		  let fData3 = document.createElement('td');
+		  let fData4 = document.createElement('td');
+		  fData1.textContent = 'ID   | Name';
+		  fData3.textContent = 'Date';
+		  fData4.textContent = 'Rating';
+		  fRow.appendChild(fData1);
+		  fRow.appendChild(fData3);
+		  fRow.appendChild(fData4);
+		  tbl.appendChild(fRow);
 		  
 		  rides.forEach(function(val, idx, rides) {
-		    console.log(val.name);
+		    count += 1;
+		    hrTotal += val.avgHr;
 		    
 		    let row = document.createElement('tr');
 		    let d1 = document.createElement('td');
-		    d1.textContent = val.name;
-		    console.log(d1.textContent);
+		    d1.textContent = "ID: " + val.id + " | " + val.name;
 		    row.appendChild(d1);
 		    row.id = val.id;
 		    d1.id = val.id;
-		    console.log(val);
 		    
 		    let d2 = document.createElement('td');
 		    d2.textContent = val.rideDate;
 		    row.appendChild(d2);
-		    
 		    let d = document.createElement('td');
 		    d.textContent = val.rating;
 		    row.appendChild(d);
 		    tbl.appendChild(row);
-		    
 		    row.addEventListener("click", rideEdit);
 		    
-		    let r = document.createElement('tr');
-		    let d3 = document.createElement('td');
-		    d3.textContent = val.description;
-		    row.appendChild(d3);
-		    tbl.appendChild(r);
-		    
-		    let row2 = document.createElement('tr');
-		    let d4 = document.createElement('td');
-		    d4.textContent = val.bike;
-		    row2.appendChild(d4);
-		    let d5 = document.createElement('td');
-		    d5.textContent = val.distance;
-		    row2.appendChild(d5);
-		    let d6 = document.createElement('td');
-		    d6.textContent = val.duration;
-		    row2.appendChild(d6);
-		    let d7 = document.createElement('td');
-		    d7.textContent = val.calories;
-		    row2.appendChild(d7);
-		    let d8 = document.createElement('td');
-		    d8.textContent = val.avgPwr;
-		    row2.appendChild(d8);
-		    let d9 = document.createElement('td');
-		    d9.textContent = val.avgHr;
-		    row2.appendChild(d9);
-		    tbl.appendChild(row2);
-		    
-		    let row3 =document.createElement('tr'); 
-		    let d10 = document.createElement('td');
-		    d10.textContent = val.courseUrl;
-		    row3.appendChild(d10);
-		    
-		    let row4 =document.createElement('tr'); 
-		    let d11 = document.createElement('td');
-		    d11.textContent = val.activityUrl;
-		    row4.appendChild(d11);
-		    
-		    let row5 =document.createElement('tr'); 
-		    let d12 = document.createElement('td');
-		    d12.textContent = val.comments;
-		    row5.appendChild(d12);
-		    
-		    let hr = document.createElement('hr');
-		    row5.appendChild(hr);
-
-		    console.log('adding event listners');
-		  });		  
-		    ridesList.appendChild(tbl);	
-
+		  });	
+		  
+		  let avgHeart = hrTotal/count;
+		  let hRateRow = document.createElement('tr');
+		  let hRateData = document.createElement('td');
+		  let hRateHead = document.createElement('h3');
+		  hRateHead.textContent = 'Average heart rate across ' + count + ' rides is:' + avgHeart; 
+		  hRateData.appendChild(hRateHead);	
+		  hRateData.colSpan = '6';
+		  hRateRow.appendChild(hRateData);	
+		  tbl.appendChild(hRateRow);	
+		  ridesList.appendChild(tbl);	
 
 	};
 	
 	function rideEdit(e) {
-		console.log(e);
 		
 		let id = e.target.id;
 		
@@ -305,7 +275,6 @@ window.addEventListener('load', function(e) {
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState === 4 && xhr.status < 400) {
 				var ride = JSON.parse(xhr.responseText);
-				console.log(ride);
 				
 				let rideEditForm = document.getElementById('rideEdit');
 				rideEditForm.id.value = ride.id;
@@ -329,6 +298,8 @@ window.addEventListener('load', function(e) {
 				rideEditForm.state.value = ride.address.state;
 				rideEditForm.zipcode.value = ride.address.zipcode;
 				
+				paintRideDetail(ride);
+				
 			};
 
 			if (xhr.readyState === 4 && xhr.status >= 400) {
@@ -341,6 +312,143 @@ window.addEventListener('load', function(e) {
 		xhr.send(null);
 		
 	};
+	
+	var paintRideDetail = (function(ride){
+		
+		let editRideDiv = document.getElementById('editRideDiv');
+		editRideDiv.textContent = '';
+		let head = document.createElement('h2');
+		head.textContent = 'Ride Detail';
+		let tblEditRide = document.createElement('table');
+		
+		  let fRow = document.createElement('tr')
+		  let fData1 = document.createElement('td');
+		  let fData3 = document.createElement('td');
+		  let fData4 = document.createElement('td');
+		  fData1.textContent = 'ID   | Name';
+		  fData3.textContent = 'Date';
+		  fData4.textContent = 'Rating';
+		  fRow.appendChild(fData1);
+		  fRow.appendChild(fData3);
+		  fRow.appendChild(fData4);
+		  let sRow = document.createElement('tr')
+		  let sData1 = document.createElement('td');
+		  sData1.textContent = 'Description';
+		  sData1.colSpan = '6';
+		  sRow.appendChild(sData1);
+		  
+		  let tRow = document.createElement('tr')
+		  let tData1 = document.createElement('td');
+		  let tData2 = document.createElement('td');
+		  let tData3 = document.createElement('td');
+		  let tData4 = document.createElement('td');
+		  let tData5 = document.createElement('td');
+		  let tData6 = document.createElement('td');
+		  tData1.textContent = 'Bike';
+		  tData2.textContent = 'Distance';
+		  tData3.textContent = 'Duration';
+		  tData4.textContent = 'Calories';
+		  tData5.textContent = 'Avg Power';
+		  tData6.textContent = 'Avg HR';
+		  tRow.appendChild(tData1);
+		  tRow.appendChild(tData2);
+		  tRow.appendChild(tData3);
+		  tRow.appendChild(tData4);
+		  tRow.appendChild(tData5);
+		  tRow.appendChild(tData6);
+		  let fourthRow = document.createElement('tr')
+		  let fourthData1 = document.createElement('td');
+		  fourthData1.textContent = 'Course URL';
+		  fourthData1.colSpan = '6';
+		  fourthRow.appendChild(fourthData1);
+		  let fifthRow = document.createElement('tr')
+		  let fifthData1 = document.createElement('td');
+		  fifthData1.textContent = 'Activity URL';
+		  fifthData1.colSpan = '6';
+		  fifthRow.appendChild(fifthData1);
+		  let sixthRow = document.createElement('tr')
+		  let sixthData1 = document.createElement('td');
+		  sixthData1.textContent = 'Comments';
+		  sixthData1.colSpan = '6';
+		  sixthRow.appendChild(sixthData1);
+		  
+		  tblEditRide.appendChild(fRow);
+		  tblEditRide.appendChild(sRow);
+		  tblEditRide.appendChild(tRow);
+		  tblEditRide.appendChild(fourthRow);
+		  tblEditRide.appendChild(fifthRow);
+		  tblEditRide.appendChild(sixthRow);
+		
+		
+		
+		
+		
+		
+		let tr = document.createElement('tr');
+		let td1 = document.createElement('td');
+		let td2 = document.createElement('td');
+		let td3 = document.createElement('td');
+		td1.textContent = "ID: " + ride.id + " | " + ride.name;
+	    td2.textContent = ride.rideDate;
+	    td3.textContent = ride.rating;
+	    tr.appendChild(td1);
+	    tr.appendChild(td2);
+	    tr.appendChild(td3);
+	    tblEditRide.appendChild(tr);
+	    let tr2 = document.createElement('tr');
+	    let td4 = document.createElement('td');
+	    td4.textContent = ride.description;
+	    td4.colSpan = '6';
+	    tr2.appendChild(td4);	    
+	    tblEditRide.appendChild(tr2);
+
+	    let tr3 = document.createElement('tr');
+	    let d4 = document.createElement('td');
+	    d4.textContent = ride.bike;
+	    tr3.appendChild(d4);
+	    let d5 = document.createElement('td');
+	    d5.textContent = ride.distance;
+	    tr3.appendChild(d5);
+	    let d6 = document.createElement('td');
+	    d6.textContent = ride.duration;
+	    tr3.appendChild(d6);
+	    let d7 = document.createElement('td');
+	    d7.textContent = ride.calories;
+	    tr3.appendChild(d7);
+	    let d8 = document.createElement('td');
+	    d8.textContent = ride.avgPwr;
+	    tr3.appendChild(d8);
+	    let d9 = document.createElement('td');
+	    d9.textContent = ride.avgHr;
+	    tr3.appendChild(d9);
+	    tblEditRide.appendChild(tr3);
+	    
+	    let tr4 =document.createElement('tr'); 
+	    let d10 = document.createElement('td');
+	    d10.textContent = ride.courseUrl;
+	    d10.colSpan = '6';
+	    tr4.appendChild(d10);
+	    tblEditRide.appendChild(tr4);
+	    
+	    let tr5 =document.createElement('tr'); 
+	    let d11 = document.createElement('td');
+	    d11.textContent = ride.activityUrl;
+	    d11.colSpan = '6';
+	    tr5.appendChild(d11);
+	    tblEditRide.appendChild(tr5);
+	    
+	    let tr6 =document.createElement('tr'); 
+	    let d12 = document.createElement('td');
+	    d12.textContent = ride.comments;
+	    d12.colSpan = '6';
+	    tr6.appendChild(d12);
+	    tblEditRide.appendChild(tr6);	    
+	    
+		editRideDiv.appendChild(tblEditRide);
+		let br = document.createElement('br');
+		editRideDiv.appendChild(br);
+		
+	});
 	
 
 	function displayRide(ride) {
@@ -356,7 +464,6 @@ window.addEventListener('load', function(e) {
 	  let desc = document.createElement('blockquote');
 	  desc.textContent = ride.description;
 	  dataDiv.appendChild(desc);
-	  console.log('appended data');
 
 	  
 	}
